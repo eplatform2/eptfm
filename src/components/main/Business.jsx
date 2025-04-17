0f6fa8374e5fff53b30c26c3eef26d65b64737d1
eplatform2 <eplatform@naver.com>
2025-04-17 10:27:34 +0900

    Update Business.jsx

--- src/components/main/Business.jsx
+++ src/components/main/Business.jsx
@@ -29,7 +29,7 @@ const Business = () => {
               hoverImg={`${process.env.PUBLIC_URL}/assets/images/main/business_img2_over.png`}
               mobile_hoverImg={`${process.env.PUBLIC_URL}/assets/images/main/business_img2_over_mobile.png`}
               text={'e-Learning'}
-              hoverText={'우리는 혁신적인 디지털 학습 솔루션을\n 통해 누구나 어디서든 쉽게 배울 수 있는\n 환경을 제공합니다.'}
+              hoverText={'가상현실(VR)과 증강현실(AR)을 통해\n 교육의 경계를 허물고, 몰입감 있는\n 학습 경험을 제공합니다.'}
             />
             <BusinessItems
               ref={(el) => (refs.current[3] = { ref: el })}
